a9420a96e4c0603aa74e267e9bf5c46e3506ebdb
a1013457richar <r11722013@g.ntu.edu.tw>
2024-01-13 17:51:01 +0800

    commit backend

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const app=express();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then((db) => console.log("db is connected"))
+  .catch((err) => console.log(err));
 
-app.listen(3000,()=>{
-    console.log('server on port 3000');
-}
-);
+const app = express();
+
+app.listen(3000, () => {
+  console.log("server on port 3000");
+});
