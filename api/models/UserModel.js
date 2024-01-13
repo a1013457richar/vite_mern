4ef4732c72cc2498537bb2888fd3cd208a48154b
a1013457richar <r11722013@g.ntu.edu.tw>
2024-01-13 17:56:36 +0800

    models

---
+++ api/models/UserModel.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      requiered: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      requiered: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
