ed983622ddd31e732980557047c7f68f00e61b29
a1013457richar <r11722013@g.ntu.edu.tw>
2024-01-13 21:54:06 +0800

    create signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,47 @@
 import User from "../models/UserModel.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-export const signup = async (req, res,next) => {
+import jwt from "jsonwebtoken";
+export const signup = async (req, res, next) => {
   //   console.log(req.body);
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password:hashPassword });
+  const newUser = new User({ username, email, password: hashPassword });
   //這裡要加await
   try {
     await newUser.save();
     res.status(201).json({
       message: "User signup successfully!",
     });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  //   console.log(req.body);
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "Email not found!"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Password not correct!"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    const { password: userPassword, ...rest } = validUser._doc;
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
