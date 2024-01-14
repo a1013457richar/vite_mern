f52ade88caf225f2d782730de3de97b1de128ce1
a1013457richar <r11722013@g.ntu.edu.tw>
2024-01-14 08:47:06 +0800

    redux persisit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import userReducer from "./use/userSlice";
-export const store=configureStore({
-  reducer: {
-    user: userReducer,
-  },
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
 
-
+export const persistor = persistStore(store);
