ce55fd4b9a217d5107ccadefaacedafb49758f64
a1013457richar <r11722013@g.ntu.edu.tw>
2024-01-15 13:22:34 +0800

    commit ui

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -3,9 +3,9 @@ import { useSelector } from "react-redux";
 import { Navigate, Outlet, Route } from "react-router-dom";
 
 const PrivateRoute = () => {
-  const { currentUser } = useSelector((state) => state.user);
+  const { userData } = useSelector((state) => state.user);
   //如果有currentUser，就顯示Outlet，否則導向/sign-in
-  return currentUser ? <Outlet /> : <Navigate to="/sign-in" replace />;
+  return userData ? <Outlet /> : <Navigate to="/sign-in" replace />;
 };
 
 export default PrivateRoute;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const { userData } = useSelector((state) => state.user);
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
+      <form className="flex flex-col gap-4">
+        <img
+          src={userData.profilePic}
+          alt="profile "
+          className="h-24 w-24 self-center cursor-pointer rounded-full object-cover "
+        />
+        <input
+          defaultValue={userData.username}
+          type="text"
+          id="username"
+          placeholder="Username"
+          className="bg-slate-100 rounded-lg p-3"
+        ></input>
+        <input
+          defaultValue={userData.email}
+          type="email"
+          id="email"
+          placeholder="Email"
+          className="bg-slate-100 rounded-lg p-3"
+        ></input>
+        <input
+          type="password"
+          id="password"
+          placeholder="Password"
+          className="bg-slate-100 rounded-lg p-3"
+        ></input>
+        <button className="bg-slate-700 text-white rounded-lg uppercase  hover:opacity-95 disabled:opacity-30 p-3">
+          Update
+        </button>
+      </form>
+      <div className="flex justify-between mt-5">
+        <span className="text-red-500 cursor-pointer">Delete Account</span>
+        <span className="text-red-500 cursor-pointer">Sign out</span>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+export default Profile;
