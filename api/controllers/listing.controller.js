bcf893bb8f89f386b497b1b62c5ad6dffcace1ba
a1013457richar <r11722013@g.ntu.edu.tw>
2024-01-17 08:45:04 +0800

    update listiung

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -23,3 +23,38 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "Not authorized"));
+  }
+  try {
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        new: true,//get the updated one
+      }
+    );
+    return res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const getListing = async (req, res, next) => {
+  
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found"));
+    }
+    return res.status(200).json(listing);
+  } catch (error) {
+    next(error);
+  }
+}
--- api/routes/listing.routes.js
+++ api/routes/listing.routes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { createListing } from "../controllers/listing.controller.js";
 import { deleteListing } from "../controllers/listing.controller.js";
+import { updateListing } from "../controllers/listing.controller.js";
+import { getListing } from "../controllers/listing.controller.js";
 import { verfyToken } from "../utils/verfyToken.js";
 const router = express.Router();
 
 router.post('/create', verfyToken,createListing)
 router.delete('/delete/:id', verfyToken,deleteListing)
+router.post('/update/:id', verfyToken,updateListing)
+router.get('/get/:id',getListing)
+
 
 export default router;
