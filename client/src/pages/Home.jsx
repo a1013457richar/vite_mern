d7b290d703ec4586f89f00d9efe1f9aa19cc9818
a1013457richar <r11722013@g.ntu.edu.tw>
2024-01-20 10:04:21 +0800

    change words

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -56,7 +56,7 @@ const Home = () => {
           place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Sahand Estate is the best place to find your next perfect place to
+          Tim Estate is the best place to find your next perfect place to
           live.
           <br />
           We have a wide range of properties for you to choose from.
